b596eed357d518280e8fcc93e70f75ade2cff2c8
Karel Lenc <lenckarel@gmail.com>
2016-03-16 17:26:44 +0000

    Added a reference to the paper about the xavier method.

--- xavier.m
+++ xavier.m
@@ -3,6 +3,11 @@ function weights = xavier(varargin)
 %   WEIGHTS = XAVIER(H, W, C, N) initializes N filters of support H x
 %   W and C channels using Xavier method. WEIGHTS = {FILTERS,BIASES}is
 %   a cell array containing both filters and biases.
+%
+% See also:
+% Glorot, Xavier, and Yoshua Bengio.
+% "Understanding the difficulty of training deep feedforward neural networks."
+% International conference on artificial intelligence and statistics. 2010.
 
 rng(1) ;
 filterSize = [varargin{:}] ;
