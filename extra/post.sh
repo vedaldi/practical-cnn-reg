37845765311eb79bc54e777394b1297c3c748e1b
Andrea Vedaldi <vedaldi@gmail.com>
2016-03-18 11:41:03 +0000

    fix packaging (again?)

--- extra/post.sh
+++ extra/post.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # A version with standard binaries
+make -f extra/Makefile distclean
 make -f extra/Makefile bins
 make -f extra/Makefile pack post post-doc
 
 # A version with EC2 binaries
+name=practical-cnn-reg-ec2 make -f extra/Makefile distclean
 name=practical-cnn-reg-ec2 make -f extra/Makefile ec2bins
 name=practical-cnn-reg-ec2 make -f extra/Makefile pack post
